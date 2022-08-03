1612c79e87c5a7175a21a06c3ffc27091ca75a74
hallelhel <hallelh@post.bgu.ac.il>
2022-08-03 19:45:10 +0300

    connected to prod

--- main.js
+++ main.js
@@ -28,11 +28,11 @@ app.use(express.static(path.join(__dirname, "public"))); //To serve static files
 //local:
 //app.use(express.static(path.join(__dirname, "dist")));
 //remote:
- app.use(express.static(path.join(__dirname, '../assignment-3-3-basic/dist')));
+ app.use(express.static(path.join(__dirname, '../assignment3-3-314943671_322917501/dist')));
 app.get("/",function(req,res)
 { 
   //remote: 
-   res.sendFile(path.join(__dirname, '../assignment-3-3-basic/dist/index.html'));
+   res.sendFile(path.join(__dirname, '../assignment3-3-314943671_322917501/dist/index.html'));
   //local:
   //res.sendFile(__dirname+"/index.html");
 
